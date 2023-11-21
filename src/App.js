e9ef29aba758eb20d1f5f203fb556d9c72795516
shaultr <a0548428751@gmail.com>
2023-11-21 15:08:04 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import KeyBoard from './components/keyboard';
+import Screen from './components/screan';
+import { useState } from 'react';
+let index = 0;
 
 function App() {
+  let [text, setText] = useState("Enter your text here");
+  const colors = ["red", "green", "blue", "pink", "orange", "purple", "black"];
+  let [color, setColor] = useState("black");
+  const changeColor = () => {
+    setColor(colors[index]);
+    index===color.length? index = 0 : index++;
+
+
+  };
+  const addtext = (e) => {
+    let newKey = e.target.textContent;
+    setText(text === "Enter your text here" ? newKey : text + newKey);
+    console.log(text)
+  };
+  const charEng = "1 2 3 4 5 6 7 8 9 0 q w e r t y u i o p a s d f g h j k l ; z x c v b n m".split(" ");
+  const charHebrew = "1234567890/'קראטוןםפשדגכעיחלךףזסבהנמצתץ".split("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Screen text={text} color={color} />
+      <KeyBoard chareng={charEng} charhebrew={charHebrew}
+        click={addtext}
+        clear={() => { setText("") }}
+        delete={() => { setText(text.substring(0, text.length - 1)) }}
+        color={changeColor} 
+        nextcolor = {colors[index]}
+        enter = { () => { setText(text + '\n'); }}
+        
+        />
     </div>
   );
 }
+
+
+
+
+
+
 
 export default App;
